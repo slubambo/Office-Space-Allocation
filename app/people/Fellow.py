1018f184e95878c5af895c2d6c1e496c6faa42d7
slubambo <slubambo57@gmail.com>
2017-04-25 15:36:57 +0300

    Tests for adding person, creating room and other things
    Application files are in the app folder, which has two folders in it so far.
    Folder Person contains the classes to do with People that is Person, Staff and Fellow
    Person tests are included in this file
    Folder Room contains the classes to do with Rooms, most of which still under progress.
    Tests to do with the room are in this folder.
    This commit also inlcudes a requirements file listing the libraries installed in the virtual environment
    The libraries so far include docopt and nose for carrying out nose tests.
    The designs folder is to have UML class designs and other diagrams yet to add

---
+++ app/people/Fellow.py
@@ -0,0 +1,4 @@
+class Fellow(object):
+    def __init__(self):
+        self.room
+        self.has_living_room
---
+++ app/people/person.py
@@ -0,0 +1,18 @@
+class Person(object):
+
+    def __init__(self):
+        self.first_name
+        self.surname
+        self.person_type
+        self.people = []
+        self.allStaff = []
+        self.allFellows = []
+
+    def create_person(self, person_type, *person_name):
+        return None
+
+    def add_person(self, type, *name):
+        return None
+
+    def edit_person(self, name):
+        return None
